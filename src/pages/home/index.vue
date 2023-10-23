2041087ea1e76cca62979507dedc91e7f154bc61
Jerry <1107965666@qq.com>
2023-10-23 23:53:42 +0800

    微改

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    HOME
+  <div class="block text-center">
+    <span class="demonstration"></span>
+    <el-carousel height="300px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
@@ -9,5 +14,23 @@
 </script>
 
 <style scoped>
+.demonstration {
+  color: var(--el-text-color-secondary);
+}
 
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
 </style>
