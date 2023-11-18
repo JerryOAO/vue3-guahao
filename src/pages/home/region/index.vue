8afb77e35161455dfb959e48d8813a06318ad809
Jerry <1107965666@qq.com>
2023-11-18 14:06:12 +0800

    地区

--- src/pages/home/region/index.vue
+++ src/pages/home/region/index.vue
@@ -3,14 +3,40 @@
     <div class="content">
       <div class="left">地区:</div>
       <ul class="regionInfo">
-        <li class="active">全部</li>
-        <li v-for="item in 20" :key="item">昌平区</li>
+        <li :class="{active:regionFlag==''}" @click="changeRegion('')">全部</li>
+        <li v-for="item in regionArr" :key="item.value" :class="{active:regionFlag==item.value}" @click="changeRegion(item.value)">{{item.name}}</li>
       </ul>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reqHospitalLevelAndRegion } from '@/api';
+import { ref,onMounted } from 'vue';
+import type { HospitalLevelAndRegionResponseData,HospitalLevelAndRegionArr } from '@/api/type';
+
+let regionFlag = ref<string>('')
+const changeRegion = (region:string) => {
+  regionFlag.value = region
+  console.log('region',region)
+}
+
+
+
+let regionArr = ref<HospitalLevelAndRegionArr>([])
+onMounted( () => {
+  getRegion()
+});
+
+const getRegion = async () => {
+ let result:HospitalLevelAndRegionResponseData = await reqHospitalLevelAndRegion('Beijin');
+ if(result.code == 200){
+   regionArr.value = result.data
+   console.log('result11',result)
+ }
+
+};
+</script>
 
 <style scoped lang="scss">
 .region {
